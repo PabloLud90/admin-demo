44e5726b1aeb275c2050d30f4fe6d0651ba10b5b
Pablo Ludeña <pabloludena@MacBook-Air-de-Pablo.local>
2020-03-31 20:07:18 -0500

    Agregamos y Modificaciones modules y  rutas en los modulos

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from "@angular/core";
+
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+
+@NgModule({
+    declarations:[
+        PagenotfoundComponent,    
+        HeaderComponent,          
+        SidebarComponent,         
+        BreadcrumbsComponent,
+
+    ],
+    exports:[
+        HeaderComponent,          
+        SidebarComponent,         
+        BreadcrumbsComponent,
+        PagenotfoundComponent,   
+    ]
+})
+
+export class SharedModule{}
